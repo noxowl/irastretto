e2d627b68aa8558b07dbe26b105e0eaa9cf5dbbd
Mirai Kim <nachtbeere@outlook.com>
2019-03-03 23:39:54 +0900

    set config on test run

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+from irastretto.config import read_irastretto_config
 from irastretto.web.app import app
 
+config = read_irastretto_config()
+app.config.update(config)
+print(app.config)
 app.run()
